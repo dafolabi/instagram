6f113de60ef951db8e15539f6645ec1195a19c8d
Daniel Afolabi <dafolabi@fb.com>
2017-06-28 11:05:38 -0700

    user can now post images and captions

--- instagram/CaptureViewController.swift
+++ instagram/CaptureViewController.swift
@@ -7,11 +7,33 @@
 //
 
 import UIKit
+import Parse
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textfield: UITextField!
+    
+    var postImage = UIImage(named: "imageName")
+    var postCaption = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+//        vc.sourceType = UIImagePickerControllerSourceType.camera
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        
+        self.present(vc, animated: true, completion: nil)
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +43,39 @@ class CaptureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        // Do something with the images (based on your use case)
+        postImage = editedImage
+        imageView.image =  editedImage
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didHitPostButton(_ sender: Any) {
+        postCaption = textfield.text ?? ""
+        Post.postUserImage(image: imageView.image, withCaption: postCaption) { (status: Bool, error: Error?) in
+            print("Sucessfully posted")
+        }
+    }
+    
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRectMake
+            (0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.scaleAspectFill
+        resizeImageView.image = image
+        
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
 
     /*
     // MARK: - Navigation
